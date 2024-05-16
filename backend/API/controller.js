8f28c690b8caba4cf0e70aa64a3b38878cc5d7ef
Anamika <202151020@iiitvadodara.ac.in>
2024-05-16 09:37:36 +0530

    fetched donar data from donar API

--- backend/API/controller.js
+++ backend/API/controller.js
@@ -8,8 +8,18 @@ const app = express(); //api
 app.use(bodyParser.json()); //api
 
 
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080'); // Update this with your frontend URL
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080'); // Update this with your frontend URL
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // Update to match your frontend URL
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
@@ -17,6 +27,7 @@ app.use((req, res, next) => {
     }
     next();
 });
+
 
 app.listen(8040, () => {
     console.log("server is now listening at 8040 port");
